071b358aca0d1cc91fc21560ee90f2e449e92e32
Kevin Lissov <kl223py@student.lnu.se>
2026-06-10 23:35:46 +0900

    Add Navbar component to App and ensure proper export in Navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Login from './pages/Login.jsx'
@@ -6,6 +7,7 @@ import Login from './pages/Login.jsx'
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,3 +11,5 @@ function Navbar() {
         </nav>
     )
 }
+
+export default Navbar
