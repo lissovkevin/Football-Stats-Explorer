c03be7c324ca8a1fa9d3ba7a8971e8559f31e1a0
Kevin Lissov <kl223py@student.lnu.se>
2026-06-11 13:01:50 +0900

    Fetch and display league data in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,40 @@ import { useEffect, useState } from 'react'
 const API_URL = 'https://football-api-quza.onrender.com/graphql'
 
 function Dashboard() {
+    const [leagues, setLeagues] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(API_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                query: `
+                query {
+                    leagues {
+                        id
+                        name
+                    }
+                }`
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            setLeagues(data.data.leagues)
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) return <p className='p-8'> Loading...</p>
     return (
         <div className="p-8">
-            <h1 className="text-3xl font-bold">Dashboard</h1>
+            <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
             <ul>
-                <li key = {leagues.id}>
-                    {leagues.name}
+                {leagues.map(league => (
+                <li key={league.id} className='py-1 text-gray-700'>
+                    {league.name}
                 </li>
+                ))}
             </ul>
         </div>
     )
