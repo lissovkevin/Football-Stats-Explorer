b392f4daf06165bb8e8019cf98a82ad8584626ba
Kevin Lissov <kl223py@student.lnu.se>
2026-06-14 20:21:33 +0900

    Add GoalsPerLeague component

---
+++ src/components/charts/GoalsPerLeague.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Bar, Chart } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const API_URL = 'https://football-api-quza.onrender.com/graphql'
+
+function GoalsPerLeague() {
+    return (
+        <div>
+            <h2>Goals Per League</h2>
+        </div>
+    )
+}
+
+export default GoalsPerLeague
