177fe1b672d21cf688e5f128a362da2343da1204
Kevin Lissov <kl223py@student.lnu.se>
2026-06-14 21:04:35 +0900

    Add state variables

--- src/components/charts/GoalsPerLeague.jsx
+++ src/components/charts/GoalsPerLeague.jsx
@@ -15,6 +15,10 @@ ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 const API_URL = 'https://football-api-quza.onrender.com/graphql'
 
 function GoalsPerLeague() {
+    const [chartData, setChartData] = useState(null)
+
+    const [loading, setLoading] = useState(true)
+
     return (
         <div>
             <h2>Goals Per League</h2>
