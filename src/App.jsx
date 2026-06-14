5575d4c800d4ba0a7667de667e6aa3e6ceaae935
Kevin Lissov <kl223py@student.lnu.se>
2026-06-14 17:42:12 +0900

    Add MatchDetail page and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Login from './pages/Login.jsx'
+import MatchDetail from './pages/MatchDetail'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/match/:id" element={<MatchDetail />} />
       </Routes>
     </BrowserRouter>
   )
