c85d56bc7a0a0a0e8cccca3c284bdcb2bb1b5756
Kevin Lissov <kl223py@student.lnu.se>
2026-06-16 13:17:00 +0900

    Add state variables to create match

--- src/pages/CreateMatch.jsx
+++ src/pages/CreateMatch.jsx
@@ -1,10 +1,22 @@
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const API_URL = 'https://football-api-quza.onrender.com/graphql'
 
 function CreateMatch() {
   const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    date: '',
+    homeTeamId: '',
+    awayTeamId: '',
+    homeGoals: 0,
+    awayGoals: 0,
+    leagueId: '',
+    season: '',
+  })
+  const [teams, setTeams] = useState([])
+  const [leagues, setLeagues] = useState([])
+  const [loading, setLoading] = useState(true)
 
   return (
     <div className="p-8">
